cdaf66be778f1924235873e61010e04b20966273
komeegbedi <oghenekomeegbedi222@gmail.com>
2021-07-05 15:11:41 -0500

    made UI cleaner

--- script.js
+++ script.js
@@ -6,34 +6,39 @@ let navBar = document.querySelector('div.collapse.navbar-collapse');
 let header = document.querySelector('nav.fixed-top');
 let navLinks = document.querySelectorAll("ul.navbar-nav li a");
 let logo = document.querySelector('.navbar-brand svg');
+let sticky = header.offsetTop + 350;
 let currentIndex = 0;
 let interval;
 
 
+//this function changes the slider image
 const changeSliderActive = (index) =>{
-   
+    
 
+    //first start by removing the current animation classes
     sliderInner[currentIndex].querySelector('h5.shoe-name').classList.remove('animate__slideInDown');
     sliderInner[currentIndex].querySelector('img').classList.remove('animate', 'animate__slideRight');
-   
     sliderInner[currentIndex].querySelector('.bottom-text').classList.remove('animate__slideInUp');
 
-
+    //then we add a fadeout animation in order to fade out smoothly 
     sliderInner[currentIndex].querySelector('h5.shoe-name').classList.add('animate__fadeOutLeft');
     sliderInner[currentIndex].querySelector('img').classList.add('animate__animated','animate__fadeOutLeft');
     sliderInner[currentIndex].querySelector('.bottom-text').classList.add('animate__fadeOutLeft');
+    indicators[currentIndex].classList.remove('active');
+    
 
-
+    //since the fadeout animation takes 1s to finish, we want to wait for that animation to be done before we slide in the next image
     setTimeout(() => {
 
-        indicators[currentIndex].classList.remove('active');
         sliderInner[currentIndex].classList.remove('active');
 
+        //remove the fadeout animation 
         sliderInner[currentIndex].querySelector('h5.shoe-name').classList.remove('animate__fadeOutLeft');
         sliderInner[currentIndex].querySelector('img').classList.remove('animate__animated', 'animate__fadeOutLeft');
         sliderInner[currentIndex].querySelector('.bottom-text').classList.remove('animate__fadeOutLeft');
 
 
+        //move to the next image and add the animation to slideIn
 
         currentIndex = index === undefined ? (currentIndex + 1) % indicators.length : index - 1;
 
@@ -47,13 +52,15 @@ const changeSliderActive = (index) =>{
     }, 1000);
 }
 
-let sticky = header.offsetTop + 350;
+
+//this function controls the header to be fixed on scroll (only on screens with width > 992 px)
 const fixedOnScroll = () => {
 
     if (window.pageYOffset > sticky && innerWidth > 992) {
 
         header.style.background = "#fff";
         logo.style.fill = "#000";
+
         navLinks.forEach(link => {
             link.style.color = "#000";
         });
@@ -62,6 +69,7 @@ const fixedOnScroll = () => {
         
         header.style.background = "transparent";
         logo.style.fill = "#fff";
+
         navLinks.forEach(link => {
             link.style.color = "#fff";
         });
@@ -69,9 +77,11 @@ const fixedOnScroll = () => {
 }
 
 const startInterval = () => {
+
     interval = setInterval(() => {
 
         changeSliderActive();
+
     }, 5000);
 }
 
@@ -99,6 +109,7 @@ menubtn.addEventListener('click' , () => {
 
 
 startInterval();
+
 onscroll = function () {
     fixedOnScroll();
 }
